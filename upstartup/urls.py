a4fad06c0372374f8960bd497236f0c929f63125
bogomazov <bogomazov1998@gmail.com>
2015-05-27 09:23:46 +0300

    Set up PostgreSQL

--- upstartup/urls.py
+++ upstartup/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls import include, patterns, url
 from views import IndexView
 
+from userprofile.views import LoginView, LogoutView
+
 urlpatterns = patterns(
     '',
-
-    url(r'^api/v1/', include(router.urls)),
-    url(r'^api/v1/', include(accounts_router.urls)),
+    url(r'^api/v1/', include('startup.urls')),
     url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
     url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
-
 
     url(r'^.*$', IndexView.as_view(), name='index'),
 )
